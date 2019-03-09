2d392b69fac6fa00d9f99702158cb02065cb4c37
shrouq <shrouqghareeb80@gmail.com>
2019-03-09 18:03:25 +0200

    added homework

---
+++ homework/day1_shrouq.js
@@ -0,0 +1,26 @@
+// function declaration
+function isSquare (number) {
+
+}
+
+
+// function assignment
+
+var isSquare = function (i) {
+    if ( i * i = n)
+    return true;
+    }
+    return false;
+}
+
+
+// your code
+
+
+       var isSquare **2 = true;
+        {
+        if ( i * i = n)
+        return true;
+        }
+        return false;
+    
