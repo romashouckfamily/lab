b0b06b0fb76a426f63b02131eb4c28f8fbfd5d8c
Melanie Gin <melaniegin1@gmail.com>
2019-03-09 15:24:39 +0200

    in class problem solving

---
+++ challenges/problemSolvingA.js
@@ -0,0 +1,26 @@
+// (1) Return all elements beginning with the letter "Q" from input array.
+// inputs: 
+var myList = ["Quit", "Quarreling", "Angry", "Joyful", "Sleepy"] 
+// input type: list of strings
+// outputs: ["Quit", "Quarreling"]
+// output type: list of strings
+
+function getElementsBeginningWithQ(list) {
+	// create a new list to store all elements beginning with Q
+	var qList = [];
+	// iterate through the list 
+	// i = i+1 is the same as i++
+	for (var i = 0; i < list.length; i++) {
+		// for each item, check if element begins with Q using an IF statement
+			if (list[i][0] == "Q") {
+				// if item begins with Q, store the element in new list
+				qList.push(list[i])	
+			}
+	}
+	// return new list		
+	//console.log("qList" + qList);
+	return qList;
+}
+
+var q = getElementsBeginningWithQ(myList);
+console.log("q: " + q);
