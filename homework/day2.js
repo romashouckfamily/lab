535f76317ea1539485d31d5ec6889a3cb5a1c15e
mahd18-meet <mahd18@meet.mit.edu>
2019-03-14 16:58:09 +0200

    Added array worksheet/homework

---
+++ homework/day2.js
@@ -0,0 +1,4 @@
+/* Take an array of numbers (for example, let a = [5, 10, 15, 20, 25])
+ and makes a new list of only the first and last elements of the given list.
+ For practice, write this code inside a function.
+*/
