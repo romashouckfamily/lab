44ade5cfb62a430011dc3925bbcefd3475abe19a
mahd18-meet <mahd18@meet.mit.edu>
2019-03-15 20:12:43 +0200

    Added day3 javascript homework file

--- homework/day2.js
+++
@@ -1,4 +0,0 @@
-/* Take an array of numbers (for example, let a = [5, 10, 15, 20, 25])
- and makes a new list of only the first and last elements of the given list.
- For practice, write this code inside a function.
-*/
