14ecc00e5ee256ab9d2fd13c11f6b322e5e5d2e3
romashoucklubov <lubovshouck4@gmail.com>
2019-03-30 06:47:20 -0400

    hi, luba

--- challenges/day_4.js
+++ challenges/day_4.js
@@ -2,6 +2,24 @@
 // Create an array of numbers
 // Create a functions that returns a new array, with only numbers less than 5 from the previous array
 // Your code here:
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+function lessthanfive(list){
+	//create an empty array to put the items less than 5
+	var newArr = [];
+	//for loops that goes through each of the item 
+	for(var i=0;i<list.length;i++){
+	//if statement that checks if each item is less than 5
+	if(list[i]<5){
+	//push item to new array
+	newArr.push(list[i]);
+	//return new array
+  }
+}
+  return newArr;
+}
+console.log(lessthanfive(array));
+
+
 
 
 
